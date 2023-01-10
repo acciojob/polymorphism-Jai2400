4172f71a25b61baaac28402d3d60913dbefeeec9
Jai <jc2000vasu@gmail.com>
2023-01-11 01:33:53 +0530

    j8

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,24 @@ package com.driver;
 
 public class Main {
 
+    public static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p=new Product();
+        int y=0, x=0,z=0;
+        double a=0.0, b=0.0;
+        p.product(x,y);
+        p.product(x,y,z);
+        p.product(a,b);
+    }
 }
